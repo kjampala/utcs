c2da1c2754d439954a3f56d45679aff1f1d8c1c0
Kaushik Jampala <kjampala@cs.utexas.edu>
2020-09-17 15:47:20 -0500

    first commit

---
+++ datalab-handout/string/my_string.c
@@ -0,0 +1,20 @@
+#include "my_string.h"
+
+
+size_t my_strlen (const char *str) 
+{
+    //your code goes here
+    return 2;
+}
+
+char *my_strncpy (char *destination, const char *source, size_t num)
+{
+    //your code goes here
+    return NULL;
+}
+
+void *my_memmove (void *destination, const void *source, size_t num)
+{
+    //your code goes here
+    return NULL;
+}
